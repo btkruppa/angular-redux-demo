571c01f986cceef9886e01c269becec9daea0518
blake kruppa <btruppa513@gmail.com>
2019-04-30 09:06:18 -0400

    added initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { NavComponent } from './components/nav/nav.component';
+import { ClickerComponent } from './components/clicker/clicker.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ClickerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
